222f619d03b603ff8ca794479240404041b4fd02
cxnkk <cenkoezkartal@gmail.com>
2025-03-16 16:34:45 +0100

    first box for the toDos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 function App() {
   return (
-    <>
-      <h1 className="bg-amber-500">Hello world</h1>
-    </>
+    <div className="flex h-screen justify-center items-center">
+      <div className="box-border rounded-md size-80 text-center border-4 bg-amber-500">
+        <h1>To do 1</h1>
+        <h1>To do 2</h1>
+        <h1>To do 3</h1>
+        <h1>To do 4</h1>
+        <h1>To do 5</h1>
+      </div>
+    </div>
   );
 }
 
